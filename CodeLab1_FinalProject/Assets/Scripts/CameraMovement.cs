c542d585903f1b7fdd17ce37ee70512b5d4512de
connorcarson <cc3237@nyu.edu>
2019-05-17 19:09:46 -0400

    Changed camera movement to center towards mouse when zooming in and center towards center of level when zooming out. Still haven't figured out how to limit camera to level size in a clean way.

--- CodeLab1_FinalProject/Assets/Scripts/CameraMovement.cs
+++ CodeLab1_FinalProject/Assets/Scripts/CameraMovement.cs
@@ -1,19 +1,29 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.IMGUI.Controls;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
     private Camera myCam;
+
+    //public GameObject level;
+    
     public float zoomMultiplier = 10f;
     public float minZoom = -7f;
     public float maxZoom = -0.75f;
+
+    private float _horizontalExtent = 12.4f;
+    private float _verticalExtent = 7f;
+
+    private Vector3 _camOrigin;
    
     // Start is called before the first frame update
     void Start()
     {
         myCam = Camera.main;
+        _camOrigin = myCam.transform.position;
     }
 
     // Update is called once per frame
@@ -33,14 +43,43 @@ public class CameraMovement : MonoBehaviour
         myCam.orthographicSize = Mathf.Clamp(myCam.orthographicSize, minZoom, maxZoom);
         
         Vector3 zoomPos = myCam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, myCam.farClipPlane));
-        
-        print(zoomPos);
 
-        if (Input.GetAxis("Mouse ScrollWheel") != 0)
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
         {
             //center camera to mouse pos
-            transform.position = Vector3.Lerp(transform.position,
+            transform.position = Vector3.Slerp(transform.position,
                 new Vector3(zoomPos.x, transform.position.y, zoomPos.z), zoomMultiplier * Time.deltaTime);
         }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            transform.position = Vector3.Slerp(transform.position, _camOrigin, zoomMultiplier * Time.deltaTime);
+        }
+        
+        /*Vector3 leftLowerBound = myCam.ViewportToWorldPoint(new Vector3(0, 0, myCam.farClipPlane));
+        Vector3 rightUpperBound = myCam.ViewportToWorldPoint(new Vector3(1, 1, myCam.farClipPlane));
+
+        print("leftlower: " + leftLowerBound);
+        print("rightupper: " + rightUpperBound);
+
+        if (leftLowerBound.x < -_horizontalExtent)
+        {
+            leftLowerBound.x = -_horizontalExtent;
+        }
+
+        if (rightUpperBound.x > _horizontalExtent)
+        {
+            rightUpperBound.x = _horizontalExtent;
+        }
+
+        if (leftLowerBound.y < -_verticalExtent)
+        {
+            leftLowerBound.y = -_verticalExtent;
+        }
+
+        if (rightUpperBound.y > _verticalExtent)
+        {
+            rightUpperBound.y = _verticalExtent;*/
+        //}
     }
 }

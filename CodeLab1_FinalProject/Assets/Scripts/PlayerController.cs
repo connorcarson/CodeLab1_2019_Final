265a142d802e79995108f1208b9e30631752876f
connorcarson <cc3237@nyu.edu>
2019-05-20 19:38:44 -0700

    Replaced art assets for cat capture UI and added player indicator UI that resizes depending on zoom of camera.

--- CodeLab1_FinalProject/Assets/Scripts/PlayerController.cs
+++ CodeLab1_FinalProject/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public NavMeshAgent agent;
+
+    public Image playerIndicator;
     
     private Camera myCam;
     
     private Vector3 _destination;
     private Vector3 _oldDest;
+
+    private RectTransform _playerIndicatorRect;
         
     // Start is called before the first frame update
     void Start()
@@ -18,6 +23,8 @@ public class PlayerController : MonoBehaviour
         myCam = Camera.main;
         _oldDest = transform.position;
         _destination = _oldDest;
+        
+        _playerIndicatorRect = playerIndicator.GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
@@ -29,10 +36,26 @@ public class PlayerController : MonoBehaviour
             _oldDest = _destination;
         }
         
-        MoveTruck();
+        MovePlayer();
+
+        Vector3 currentPos = myCam.WorldToScreenPoint(transform.position);
+        Vector3 currentPOs2 = myCam.WorldToScreenPoint(transform.forward);
+
+        playerIndicator.transform.position = currentPos;
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            _playerIndicatorRect.sizeDelta = new Vector2(_playerIndicatorRect.sizeDelta.x - 200f * Time.deltaTime, _playerIndicatorRect.sizeDelta.y - 200f * Time.deltaTime);
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            _playerIndicatorRect.sizeDelta = new Vector2(_playerIndicatorRect.sizeDelta.x + 200f * Time.deltaTime, _playerIndicatorRect.sizeDelta.y + 200f * Time.deltaTime);
+        }
+
+        _playerIndicatorRect.sizeDelta = new Vector2(Mathf.Clamp(_playerIndicatorRect.sizeDelta.x, 100, 300), Mathf.Clamp(_playerIndicatorRect.sizeDelta.y, 100, 300));
     }
 
-    void MoveTruck()
+    void MovePlayer()
     {
         if (Input.GetMouseButtonDown(0))
         {
